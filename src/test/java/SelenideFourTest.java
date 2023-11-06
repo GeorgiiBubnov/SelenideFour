9031bbbc22e6cb79584040c840c197ccdc1bd37a
GeorgiiBubnov <georgii.bubnov.spb@gmail.com>
2023-11-06 19:40:36 +0300

    FinalTest

---
+++ src/test/java/SelenideFourTest.java
@@ -0,0 +1,42 @@
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Selenide.$;
+
+public class SelenideFourTest {
+
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.browserSize = "1920x1080";
+        Configuration.baseUrl = "https://github.com/";
+        Configuration.pageLoadStrategy = "eager";
+
+
+    }
+
+    @Test
+    void gitHubTest () {
+        open ("/selenide/selenide");
+        $ ("#wiki-tab").click();
+        $ (".js-wiki-more-pages-link").click();
+        $ (".wiki-rightbar").shouldHave(text("SoftAssertions"));
+        $ (byText("SoftAssertions")).click();
+        $ ("#user-content-3-using-junit5-extend-test-class").sibling(0).shouldHave(text("@ExtendWith({SoftAssertsExtension.class})\n" +
+                "class Tests {\n" +
+                "  @Test\n" +
+                "  void test() {\n" +
+                "    Configuration.assertionMode = SOFT;\n" +
+                "    open(\"page.html\");\n" +
+                "\n" +
+                "    $(\"#first\").should(visible).click();\n" +
+                "    $(\"#second\").should(visible).click();\n" +
+                "  }\n" +
+                "}"));
+
+    }
+}
+
